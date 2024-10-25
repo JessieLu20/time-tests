7acc8f3ccae097112df19066005663c4f327731d
hanlu <ucaplub@ucl.ac.uk>
2024-10-25 18:28:49 +0100

    unit test+bug fixing

--- times.py
+++ times.py
@@ -21,9 +21,11 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            overlap_time.append((low, high))
+            # bug fixing: only overlapping exists, below min/max range satisfy
+            if start2<=end1 and end2>=start1:
+                low = max(start1, start2)
+                high = min(end1, end2)
+                overlap_time.append((low, high))
     return overlap_time
 
 if __name__ == "__main__":
